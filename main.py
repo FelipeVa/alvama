0ed314f1762275a76b2c1ec19430fcc2697558d3
Felipe Valencia <pipeedev@gmail.com>
2022-07-25 13:36:38 -0500

    chanegs related to alvama gui and server

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from pulp import LpProblem, LpMinimize, LpVariable, LpStatus, value, lpSum, LpInteger, makeDict, LpContinuous
-from pandas import DataFrame
 import json
+import os
+import sys
+import time
+from pathlib import Path
+
+from pulp import LpProblem, LpMinimize, LpVariable, lpSum, LpInteger, PULP_CBC_CMD, value, LpStatus
 
 
 def json_loader(file_name):
-    file = f"data/{file_name}.json"
+    file = Path(os.path.join(os.path.dirname(__file__), f"data/{file_name}.json"))
 
     with open(file) as json_file:
         data = json.loads(json_file.read())
@@ -12,11 +16,11 @@ def json_loader(file_name):
 
 
 def get_total_cycles(cycle_time):
-    return round(20 * 60 / cycle_time)
+    return round(20 * 60 / int(cycle_time))
 
 
 def get_cycle_cost(cost_per_km, length):
-    return cost_per_km * length * 2
+    return float(cost_per_km) * float(length) * 2
 
 
 def get_total_cost_bus_per_day(cost_per_km, length, cycle_cost):
@@ -24,8 +28,10 @@ def get_total_cost_bus_per_day(cost_per_km, length, cycle_cost):
 
 
 def main():
+    start_time = time.time()
+    json_arg = sys.argv[1] if len(sys.argv) > 1 else False
     # load data from json file
-    data = json_loader("data")
+    data = json.loads(json_arg) if json_arg else json_loader("data_2")
 
     # Routes information
     routes = data['routes']
@@ -45,13 +51,12 @@ def main():
     Tiene como objetivo minimizar los costes totales, ademas de minimizar el numero de buses que se utilizan
     
     Min Z = sum((for each route i) (for each bus j) (for each capacity k) c[i][j][k] * Y[i][j][k] * X[i][j][k])
-    
-    
     """
 
     # Funcion objetivo X[i][j][k] Y[i][j][k] * c[i][j][k]
     problem += lpSum(
-        [X[i][j][k] * get_total_cost_bus_per_day(buses[j]['costs']['per_km'], routes[i]['length'], routes[i]['cycle_time'])
+        [X[i][j][k] * get_total_cost_bus_per_day(buses[j]['cost_per_km'], routes[i]['length'],
+                                                 routes[i]['cycle_time'])
          for i in range(len(routes)) for j in range(len(buses)) for k in
          range(len(buses[j]['capacities']))]), 'Numero de buses '
 
@@ -59,46 +64,53 @@ def main():
 
     for j in range(len(buses)):
         for k in range(len(buses[j]['capacities'])):
-            problem += lpSum([X[i][j][k] for i in range(len(routes))]) <= buses[j]['capacities'][k]['available']
+            problem += lpSum([X[i][j][k] for i in range(len(routes))]) <= int(buses[j]['capacities'][k]['available'])
 
     # sum{ i, j } Kjk*Yijk*Xijk >= di;  for i
     for i in range(len(routes)):
         problem += lpSum(
-            [buses[j]['capacities'][k]['capacity'] * get_total_cycles(routes[i]['cycle_time']) * X[i][j][k] for j in
-             range(len(buses)) for k in range(len(buses[j]['capacities']))]) >= routes[i]['demand']
-
-    # The problem data is written to an .lp file
-    problem.writeLP("alvama.lp")
+            [int(buses[j]['capacities'][k]['capacity']) * get_total_cycles(routes[i]['cycle_time']) * X[i][j][k] for j in
+             range(len(buses)) for k in range(len(buses[j]['capacities']))]) >= int(routes[i]['demand'])
 
     # The problem is solved using PuLP's choice of Solver
-    problem.solve()
+    problem.solve(PULP_CBC_CMD(msg=False))
 
     # The status of the solution is printed to the screen
     buses_count = 0
     buses_group = []
 
     for v in problem.variables():
-        bus_group_split = v.name.split('_')
-
         if v.varValue != 0:
-            buses_group.append({
-                'route': int(bus_group_split[2]) + 1,
-                'type': int(bus_group_split[3]) + 1,
-                'capacity': int(bus_group_split[4]) + 1,
-                'amount': v.varValue
-            })
+            bus_group_split = v.name.split('_')
+            route = routes[int(bus_group_split[2])]
+            bus = buses[int(bus_group_split[3])]
+            capacity = bus['capacities'][int(bus_group_split[4])]
+
+            if json_arg:
+                buses_group.append({
+                    'route_id': route['id'],
+                    'bus_id': bus['id'],
+                    'capacity_id': capacity['id'],
+                    'amount': v.varValue,
+                })
+            else:
+                buses_group.append({
+                    'route': int(bus_group_split[2]) + 1,
+                    'type': int(bus_group_split[3]) + 1,
+                    'capacity': int(bus_group_split[4]) + 1,
+                    'amount': v.varValue
+                })
 
         buses_count += v.varValue
 
-    buses_data_frame = DataFrame(data=buses_group).set_index('route')
-    # buses_data_frame['sum'] = buses_data_frame[['A', 'B', 'C']].sum(axis=1)
-    print("Total buses to be used: ", buses_count)
-    print("Buses group: \n", buses_data_frame.sort_values(by='route'))
-    # Se imprime el status del problema
-    print("Status:", LpStatus[problem.status])
+    response = {
+        'status': LpStatus[problem.status],
+        'objective': round(value(problem.objective), 4),
+        'time': round(time.time() - start_time, 4),
+        'results': buses_group
+    }
 
-    # Se imprime la funciónobjetivo
-    print("funcion_objetivo", value(problem.objective))
+    return print(json.dumps(response))
 
 
 if __name__ == '__main__':
